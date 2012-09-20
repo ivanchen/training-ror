d6ba6e15a1647ec92b96b671b42b7c6d0745c730
IvanChen <ivan.andhika.c@gmail.com>
2012-09-20 13:26:00 +0700

    session one

---
+++ sessionOne/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '7609837143093c11c89512b9309057061550873fc470218c6df996cca3d01fd17e9cc304c5e8c4613ffd47bc34288426f6bf8d084aaa70ad459c736a2cfed012'
