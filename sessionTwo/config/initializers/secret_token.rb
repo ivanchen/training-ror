6463d76ee4051077e7b8b24696262bcb9a3e0f0a
IvanChen <ivan.andhika.c@gmail.com>
2012-09-21 13:36:26 +0700

    session 2

---
+++ sessionTwo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SessionTwo::Application.config.secret_token = 'b5106df3ff37c01d781396ace17b96b2903e0e10da11958403b24282368f35dec59319bee082c36abf701644fe27a952db71f468e09e2ac5601ddaacd4c8fd93'
