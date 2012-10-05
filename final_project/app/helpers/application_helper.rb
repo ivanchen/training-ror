a4e35b8fc27bcc2cf2f09c25cfe33824a01e3a8b
IvanChen <ivan.andhika.c@gmail.com>
2012-10-05 14:06:23 +0700

    add tree of category

--- final_project/app/helpers/application_helper.rb
+++ final_project/app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
 	    str =  link_to "#{current_user.name} - #{current_user.email}", edit_user_path(current_user.id)
 	    str += link_to "Logout", log_out_path
 	  else
-	    str = "#{link_to "Login", log_in_path} "
-	    str += link_to "Signup", sign_up_path
+	    # str = "#{link_to "Login", log_in_path} "
+	    # str = link_to "Signup", sign_up_path
 	  end
 	end 
 end
--- final_project/app/models/category.rb
+++ final_project/app/models/category.rb
@@ -2,4 +2,11 @@ class Category < ActiveRecord::Base
   attr_accessible :id, :name, :parent_id
   has_many :products_categories
   has_many :products
+   scope :child, lambda {|parent_id| where("parent_id = ?", parent_id) }
+
+  def self.getParent
+  	@catogories = Category.find_all_by_parent_id("")
+  	return @catogories
+  end
+ 
 end
